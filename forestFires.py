791de111a09c1a322e4a27162d67db12d4b10ae2
MichelleMarano <72081450+MichelleMarano@users.noreply.github.com>
2020-11-30 15:05:28 -0500

    mitchchanges

--- forestFires.py
+++ forestFires.py
@@ -1,2 +1,19 @@
 #Program Purpose
-#This program creates a streamlined way for users to input fire point data in order to determine wildfire hotspots
+#This program will allow the user to evaluate forest re activity in the region chosen. You Will upload a CSV le, the program will output cluster information to about hotspots and re behavior by classifying the dierent cluster type i.e clumped, dispersed, random. 
+import csv
+
+print("Data Input from Modis Forest Fire Occurences")
+print()
+print("Please input your boundary Shapefile for area of analysis")
+print()
+input("")
+
+def main():
+    Flie = open (r"blah.csv", "r")
+    datafile = csv.reader(Flie)
+
+    newfile = open (r"exported_data.csv", "w")
+    writer = csv.writer(newfile)
+
+    header = next(datafile)
+    writer.writerow(header+["", ""])
