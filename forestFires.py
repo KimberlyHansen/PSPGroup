9a361396667b45df16229cbbed7f63854f37b33d
MichelleMarano <72081450+MichelleMarano@users.noreply.github.com>
2020-12-03 08:33:29 -0500

    michellesChanges

--- forestFires.py
+++ forestFires.py
@@ -55,7 +55,21 @@ arcpy.stats.DensityBasedClustering(points, r"C:\GEOM67\GroupProject\pointCluster
 "HDBSCAN", 15) 
 # addiotnal parameters for DBSCAN and OPTICS: ({search_distance}, cluster_sensitivity)
 
+#Left to do 
 
+#SELECT points contained within the boundary area of interest
+
+#CREATE clusters of the points contained within the area using Density
+
+#based Clustering geoprocessing tool (Multi-scale (OPTICS) method) using first cluster distance
+
+#CREATE clusters within each area using Density-based Clustering geoprocessing tool using second cluster distance
+
+#REPEAT for each area of interest
+
+#CLASSIFY clusters based on outputs, specified cluster distance
+
+#RECLASSIFY nosie values as random points
 
 
 print ("Outputs for Forest fire occurrences in your region:  ") 
@@ -64,6 +78,8 @@ print()
 print("Cluster Data: ")
 # print columns
 print()
+
+#DISPLAY type, number of point clumps within each area (table and/or map), number of points in each cluster, number of random points
 
 print("Total amount of clusterd points clumped", clumpedData " and total dispersed", dispersedData )
 print("Number of areas and points analyed over", inputyear "years in", inputRegion "region")
